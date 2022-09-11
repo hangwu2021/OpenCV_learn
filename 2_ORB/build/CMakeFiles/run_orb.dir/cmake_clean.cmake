a27c2f88f20e953602d5b482e7c19a88513fe45a
hangwu2021 <hangwu2021@outlook.com>
2022-09-10 23:50:26 -0700

    CV

---
+++ 2_ORB/build/CMakeFiles/run_orb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_orb.dir/test/run_orb.cpp.o"
+  "run_orb"
+  "run_orb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_orb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
